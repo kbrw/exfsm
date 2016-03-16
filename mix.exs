e5120b53c7afeec9251d6354ce2faafaec513d9a
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2016-03-16 23:16:49 +0100

    new hex version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExFSM.Mixfile do
 
   def project do
     [ app: :exfsm,
-      version: "0.0.3",     
+      version: "0.1.0",     
       elixir: "~> 1.2.0",     
       build_embedded: Mix.env == :prod,
       consolidate_protocols: Mix.env != :test,
@@ -16,10 +16,11 @@ defmodule ExFSM.Mixfile do
         Simple elixir library to define composable FSM as function
         (not related at all with `:gen_fsm`, no state/process management)
       """,
-      package: [links: %{"Source"=>"http://github.com/awetzel/exfsm",
-                         "Doc"=>"http://hexdocs.pm/exfsm"},
-                contributors: ["Arnaud Wetzel"],
-                licenses: ["MIT"]],
+     package: [
+       maintainers: ["Arnaud Wetzel"],
+       licenses: ["MIT"],
+       links: %{"GitHub" => "https://github.com/awetzel/exfsm", "Doc"=>"http://hexdocs.pm/exfsm"}
+     ],
       deps: [{:ex_doc, only: :dev}] ]
   end
 end
