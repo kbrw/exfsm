60f903ebfeca8c7876874116826e821c44b2c489
Krapaince <Krapaince@pm.me>
2025-09-15 15:14:58 +0200

    chore: run mix format

--- mix.exs
+++ mix.exs
@@ -2,27 +2,31 @@ defmodule ExFSM.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :exfsm,
+    [
+      app: :exfsm,
       version: "0.1.6",
       elixir: ">= 1.2.0",
-      build_embedded: Mix.env == :prod,
-      consolidate_protocols: Mix.env != :test,
+      build_embedded: Mix.env() == :prod,
+      consolidate_protocols: Mix.env() != :test,
       docs: [
         main: "ExFSM",
-        source_url: "https://github.com/awetzel/exfsm",
+        source_url: "https://github.com/kbrw/exfsm",
         source_ref: "master"
       ],
       description: """
         Simple elixir library to define composable FSM as function
         (not related at all with `:gen_fsm`, no state/process management)
       """,
-     package: [
-       maintainers: ["Arnaud Wetzel"],
-       licenses: ["MIT"],
-       links: %{"GitHub" => "https://github.com/awetzel/exfsm", "Doc"=>"http://hexdocs.pm/exfsm"}
-     ],
+      package: [
+        maintainers: ["Arnaud Wetzel"],
+        licenses: ["MIT"],
+        links: %{
+          "GitHub" => "https://github.com/kbrw/exfsm",
+          "Doc" => "http://hexdocs.pm/exfsm"
+        }
+      ],
       deps: [
-	      {:ex_doc, ">= 0.0.0", only: :dev},
+        {:ex_doc, ">= 0.0.0", only: :dev}
       ]
     ]
   end
